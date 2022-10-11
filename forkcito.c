8bc5a614264ae824cbb5e1116fa95844436e4300
Sebastian Valencia Zapata <sevalenciaz@unal.edu.co>
2022-10-11 01:42:42 -0500

    Probando paso a paso en forkcito

--- forkcito.c
+++ forkcito.c
@@ -2,26 +2,53 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {c
 
-    // Creacion proceso hijo
+    // Creacion de arreglos para las tuberias
+    int hijo_a_padre[2];
+    int padre_a_hijo[2];
+
+    // Manejo de excepciones al crear las tuberias
+    if (pipe(hijo_a_padre) < 0){
+        printf("Error al crear la tuberia 1");
+        return 1;
+    }
+    if (pipe(padre_a_hijo) < 0){
+        printf("Error al crear la tuberia 2");
+        return 2;
+    }
+
+    // Creacion del proceso hijo
     int rc = fork();
 
     if (rc < 0){
         
         // Falla creacion proceso hijo
         printf("Fallo fork()\n");
-        exit(1);
+        return 2;
     
     } else if (rc == 0){
         
-        
+        // PROCESO HIJO
     
     } else {
         
-        // Proceso padre sigue por aqui
-        printf("Proceso padre de (pid:%d)\n", rc);
-    
+        // PROCESO PADRE
+
+        // Para guardar el texto ingresado por usuario y recibido por hijo
+        char texto_enviado_padre[200];
+        char texto_recibido_padre[200];
+
+        // Se pide al usuario que ingrese el texto
+        printf("Ingrese cadena de texto: ");
+        
+        // Se guarda el texto ingresado
+        fgets(texto_enviado_padre, sizeof(texto_enviado_padre), stdin);
+        
+        // Se elimina el salto de línea (Cuando el usuario da ENTER)
+        texto_enviado_padre[strlen(texto_enviado_padre) - 1] = '\0';
+
+
     }
 
 }
