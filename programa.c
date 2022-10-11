1c235c1afac63dd6b16128ad7d7ad64186a7bc9a
Alejandra Uribe <aluribes@unal.edu.co>
2022-10-11 00:00:09 -0500

    Update programa.c
    Hice muchas cosas y no sé qué hicexd

--- programa.c
+++ programa.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
+
+    // Creacion de las tuberias
+    int padre_a_hijo[2];
+    int hijo_a_padre[2];
+    pipe(padre_a_hijo);
+    pipe(hijo_a_padre);
+
+    // Creacion de variables
+    // char envio_padre[200]; 
+    // char recibe_padre[200]; 
+    // char envio_hijo[200]; 
+    // char recibe_hijo[200];
 
     // Creacion proceso hijo
     int rc = fork();
@@ -13,7 +26,11 @@ int main(int argc, char *argv[]) {
         return(1);
     } else if (rc == 0){
         // Proceso hijo
-        read(fd[0],buffer,sizeof(buffer)); //leo el pipe
+        dup2(fd[1], 1);
+        close(fd[0]);
+        dup2(fd2[0], 0);
+        close(fd2[1]);
+        execlp("clang-format", "clang-format", (char *)NULL);
         printf("Proceso hijo recibe: %s", name);
         for (int indice = 0; name[indice] != '\0'; ++indice){
             name[indice] = toupper(name[indice]);
